fe95999bbf4aee15d9e61ee38a5200c601326089
kedarjoyner <kedarjoyner@gmail.com>
2015-10-07 18:46:28 -0500

    working on counting

--- js/app.js
+++ js/app.js
@@ -22,34 +22,35 @@ $(document).ready(function() {
 		var input = $(".input-box").val(); //grabs value of current item user inputs //why some of paranthenses and others not?
 		var icons = '<span class="icons"><i class="fa fa-square-o"></i></i><i class="fa fa-minus-circle"></i></span>';	
 		event.preventDefault(); // prevents page reload after submission. Why does this happen? Why do I need "event"
-		if (input === '') { //if user enters nothing
-			alert("Oops! You forgot to enter an item.");
-		}
-			else { //submit
-				$(".list-items").prepend("<li>" + input + icons + "</li>");
-				$(".input-box").val(''); //removes text within input box
-				updateParagraph();
-			}	
+			if (input === '') { //if user enters nothing
+				alert("Oops! You forgot to enter an item.");
+			}
+				else { //submit
+					$(".list-items").prepend("<li>" + input + icons + "</li>");
+						$(".input-box").val(''); //removes text within input box
+							updateParagraph();
+				}	
 	});
 
     $('.list-items').on('click', '.fa-minus-circle', function() { //why can't this be $(trash).click.etc...?
         $(this).closest('li').remove();
-        updateParagraph();
+        	updateParagraph();
     });
 
 
     $('.list-items').on('click', '.fa-square-o', function() { //click on square and change to checked
         $(this).toggleClass('fa-check-square-o fa-square-o'); 
         	$(this).closest('li').css('opacity', 0.2).css('background-color', '#cd5b45').css('text-decoration', 'line-through').appendTo('ul');
-        	if ($(this).hasClass('fa-check-square-o')) {
-				$(".update-paragraph").text("You have " + itemCount + " items left.");
-        	}
+	        	if ($(this).hasClass('fa-check-square-o')) {
+	        		itemCount--;
+	        			$(".update-paragraph").text("You have " + itemCount + " item left.");
+	        	}  	
   	
     });
 
     $('.list-items').on('click', '.fa-check-square-o', function() { //click on check and change back to unchecked
         $(this).toggleClass('fa-check-square-o fa-square-o');
-        $(this).closest('li').css('opacity', "").css('background-color', "").css('text-decoration', 'none').prependTo('ul');
+        	$(this).closest('li').css('opacity', "").css('background-color', "").css('text-decoration', 'none').prependTo('ul');
 
     });    
 });
